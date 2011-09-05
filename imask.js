d899481f84e08516168ee176af8ada3115c3a8ab
Alex Drummond <a.d.drummond@gmail.com>
2011-09-05 17:05:33 +0100

    Minor.

--- imask.js
+++ imask.js
@@ -383,7 +383,7 @@ Imask.prototype._retrieveFromImap = function(username, sinceDateString, callback
         })
         .flatten()
         .parMap_(function (this_, id, index) {
-            opts.log('info', "Fetching message " + id + "for " + imapservername(opts, username));
+            opts.log('info', "Fetching message " + id + " for " + imapservername(opts, username));
             this.vars.id = id;
             imap.fetch(id, { request: { headers: true, body: false, struct: false }}).on('message', function (m) {
                 imap.fetch(id, { request: { headers: false, body: true, struct: false }}).on('message', function (m2) {
